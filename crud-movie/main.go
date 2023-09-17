5217b0ba1c4d949db724885df5eec59b821c119d
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-09-17 07:08:40 +0700

    make route and controller

--- crud-movie/main.go
+++ crud-movie/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"crud-movie/controllers/productcontroller"
+	"crud-movie/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("this is project crud-movie")
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/products", productcontroller.Index)
+	r.GET("/api/products/:id", productcontroller.Show)
+	r.POST("/api/product", productcontroller.Create)
+	r.PUT("/api/product/:id", productcontroller.Update)
+	r.DELETE("/api/product", productcontroller.Delete)
+
+	r.Run(":8181")
 }
--- crud-movie/models/setup.go
+++ crud-movie/models/setup.go
@@ -1,22 +1,20 @@
 package models
 
 import (
-	"fmt"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func conn() {
-
-	fmt.Println("hellp")
-	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/phabricator_user"))
+func ConnectDatabase() {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/study_go"))
 
 	if err != nil {
 		panic(err)
 	}
+
+	db.AutoMigrate(&Product{})
 
 	DB = db
 }
