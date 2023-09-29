cad13823a1d229b5221654d93ef9a6112841ec22
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-09-29 11:07:34 +0700

    add models struct

--- crud-book/models/book.go
+++ crud-book/models/book.go
@@ -1,13 +1,21 @@
 package models
 
+import "time"
+
 type Book struct {
 	//book table
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type Category struct {
 	//category table
+	Name string `json:"name"`
 }
 
 type Author struct {
 	//author table
+	Name string `json:"name"`
 }
