beed6a4bd842718aa917028c0369964c3427138a
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-10-01 06:00:06 +0700

    add gin and rethink project folders

--- crud-book/main.go
+++ crud-book/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"crud-book/db"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDb()
+
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test health",
+		})
+	})
+
+	r.Run()
 }
