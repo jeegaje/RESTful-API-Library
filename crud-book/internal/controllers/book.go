1df36048022a3260938e5bfe576306bab1044d64
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-10-04 05:14:09 +0700

    change response in get all books

--- crud-book/internal/controllers/book.go
+++ crud-book/internal/controllers/book.go
@@ -17,10 +17,27 @@ func GetAllBooks(c *gin.Context) {
 
 	for _, book := range books {
 		response := map[string]any{
-			"title":      book.Title,
-			"descripton": book.Description,
-			"author":     book.Author.FirstName,
-			"genre":      book.Genre.Name,
+			"title":           book.Title,
+			"descripton":      book.Description,
+			"bookLanguage":    book.BookLanguage,
+			"publicationYear": book.PublicationYear,
+			"publisher":       book.Publisher,
+			"isbn":            book.Isbn,
+			"pageCount":       book.PageCount,
+			"price":           book.Price,
+			"availability":    book.Availability,
+			"averageRating":   book.AverageRating,
+			"author": map[string]any{
+				"firstName":   book.Author.FirstName,
+				"lastName":    book.Author.LastName,
+				"birthDate":   book.Author.BirthDate,
+				"nationality": book.Author.Nationality,
+				"email":       book.Author.Email,
+			},
+			"genre": map[string]any{
+				"ID":   book.Genre.ID,
+				"name": book.Genre.Name,
+			},
 		}
 		responses = append(responses, response)
 	}
