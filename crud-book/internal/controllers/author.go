090f0ca088cd9b4b22a045b046460cb68dd80e39
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-10-04 05:01:58 +0700

    add endpoint get all authors

--- crud-book/internal/controllers/author.go
+++ crud-book/internal/controllers/author.go
@@ -1,13 +1,32 @@
 package controllers
 
 import (
+	"crud-book/db"
+	"crud-book/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllAuthors(c *gin.Context) {
+	var authors []*models.Author
+	var responses []map[string]any
+
+	db.DB.Find(&authors)
+
+	for _, author := range authors {
+		response := map[string]any{
+			"firstName":   author.FirstName,
+			"lastName":    author.LastName,
+			"birthDate":   author.BirthDate,
+			"nationality": author.Nationality,
+			"email":       author.Email,
+		}
+		responses = append(responses, response)
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Success access authors",
+		"code": 200,
+		"data": responses,
 	})
 }
