f49d0a1fc963953aa38efaacd3aa8b7c70cfa3f6
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-10-04 04:52:23 +0700

    add endpoint get all genres

--- crud-book/internal/controllers/genre.go
+++ crud-book/internal/controllers/genre.go
@@ -1,13 +1,27 @@
 package controllers
 
 import (
+	"crud-book/db"
+	"crud-book/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetAllGenres(c *gin.Context) {
+	var genres []*models.Genre
+	var responses []map[string]any
+
+	db.DB.Find(&genres)
+	for _, genre := range genres {
+		response := map[string]any{
+			"ID":   genre.ID,
+			"name": genre.Name,
+		}
+		responses = append(responses, response)
+	}
 	c.JSON(http.StatusOK, gin.H{
-		"message": "Success connect to Author Controllers",
+		"code": 200,
+		"data": responses,
 	})
 }
